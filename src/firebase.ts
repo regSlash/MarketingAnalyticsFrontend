ee9acdad8704155fdb5fcc871dcbc08c4df10d8a
regSlash <ed.koshman@gmail.com>
2025-05-20 19:15:30 +0000

    something

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, RecaptchaVerifier, Auth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRfw0uNxq7OMYLokj3cQUyc_PZP7Hxd7I",
+  apiKey: "API",
   authDomain: "marketing-analytics-e0743.firebaseapp.com",
   projectId: "marketing-analytics",
   messagingSenderId: "1234567890",
